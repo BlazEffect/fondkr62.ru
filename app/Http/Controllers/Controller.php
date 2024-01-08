cc4a91e95e297d60e3973203a8dd28a28ffc9713
BlazEffect <kamalinigor@gmail.com>
2024-01-08 22:56:36 +0300

    Made the main menu structure

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,113 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    protected array $vars;
+
+    public function __construct()
+    {
+        // Temporary solution for menu display
+        $this->vars['menu'] = [
+            [
+                'title' => 'О фонде',
+                'link' => '',
+                'children' => [
+                    [
+                        'title' => 'Общая информация',
+                        'link' => '',
+                    ],
+                    [
+                        'title' => 'Термины и определения',
+                        'link' => '',
+                    ],
+                    [
+                        'title' => 'Реквизиты фонда',
+                        'link' => '',
+                    ],
+                    [
+                        'title' => 'Сведения о руководстве',
+                        'link' => '',
+                    ],
+                    [
+                        'title' => 'Учредительные документы',
+                        'link' => '',
+                    ],
+                    [
+                        'title' => 'Документы фонда',
+                        'link' => '',
+                    ],
+                    [
+                        'title' => 'Финансовая устойчивость',
+                        'link' => '',
+                    ],
+                    [
+                        'title' => 'Контролирующие органы',
+                        'link' => '',
+                    ]
+                ]
+            ],
+            [
+                'title' => 'Нормативно-правовая база',
+                'link' => '',
+                'children' => [
+                    [
+                        'title' => 'Федеральное законодательство',
+                        'link' => '',
+                        'children' => [
+                            [
+                                'title' => 'Законы',
+                                'link' => '',
+                            ],
+                            [
+                                'title' => 'Приказы',
+                                'link' => '',
+                            ],
+                            [
+                                'title' => 'Постановления',
+                                'link' => '',
+                            ],
+                            [
+                                'title' => 'Регламенты и рекомендации',
+                                'link' => '',
+                            ]
+                        ]
+                    ],
+                    [
+                        'title' => 'Региональное законодательство',
+                        'link' => '',
+                        'children' => [
+                            [
+                                'title' => 'Законы',
+                                'link' => '',
+                            ],
+                            [
+                                'title' => 'Постановления',
+                                'link' => '',
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+            [
+                'title' => 'Отчётность',
+                'link' => '',
+            ],
+            [
+                'title' => 'Новости',
+                'link' => '',
+            ],
+            [
+                'title' => 'Проведение каремонта',
+                'link' => '',
+            ],
+            [
+                'title' => 'Отзывы',
+                'link' => '',
+            ],
+            [
+                'title' => 'Контакты',
+                'link' => '',
+            ]
+        ];
+    }
 }
