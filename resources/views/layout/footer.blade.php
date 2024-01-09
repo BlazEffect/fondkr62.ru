62cdd82b6cb0a5d1c77ae2b9612e2c1c5a3aadbc
BlazEffect <kamalinigor@gmail.com>
2024-01-09 18:48:21 +0300

    Added footer

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,113 @@
+<footer class="footer">
+    <div class="footer__container">
+        <div class="footer__wrapper">
+            <div class="footer-map">
+                <iframe
+                    src="https://yandex.ru/map-widget/v1/?from=mapframe&ll=39.750983%2C54.614135&mode=poi&poi%5Bpoint%5D=39.750981%2C54.614166&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D1332193447&z=19.87"
+                    width="438" height="290">
+                </iframe>
+            </div>
+
+            <div class="footer-contacts">
+                <div class="footer-contacts__address">
+                    <div class="footer-contacts__address-text--bolder">
+                        Адрес:
+                    </div>
+
+                    <div class="footer-contacts__address-text--normal">
+                        390046, г. Рязань, ул. Маяковского, д. 1а, строение 3 (корпус Е)
+                    </div>
+                </div>
+
+                <div class="footer-contacts__phone--PTO">
+                    <div class="footer-contacts__phone-text--bolder">
+                        Отдел ПТО:
+                    </div>
+
+                    <div class="footer-contacts__phone-text--normal">
+                        <a href="tel:4912465219">
+                            (4912) 46-52-19
+                        </a>
+                    </div>
+                </div>
+
+                <div class="footer-contacts__phone--subscription">
+                    <div class="footer-contacts__phone-text--bolder">
+                        Абонентский отдел:
+                    </div>
+
+                    <div class="footer-contacts__phone-text--normal">
+                        <a href="tel:4912465190">
+                            (4912) 46-51-90
+                        </a>
+                    </div>
+                </div>
+
+                <div class="footer-contacts__scheduler">
+                    <div class="footer-contacts__scheduler-text--bolder">
+                        Режим приема звонков и личного приема собственников помещений абонентским отделом:
+                    </div>
+
+                    <div class="footer-contacts__scheduler-text--normal">
+                        пн-чт: 8:00 - 15:00,<br>
+                        пт: неприемный день,<br>
+                        сб-вс: выходные дни,<br>
+                        обеденный перерыв: 12:00 - 12:45
+                    </div>
+                </div>
+
+                <div class="footer-contacts__phone--interaction">
+                    <div class="footer-contacts__phone-text--bolder">
+                        Отдел по взаимодействию с собственниками помещений и ОМСУ:
+                    </div>
+
+                    <div class="footer-contacts__phone-text--normal">
+                        <a href="tel:4912465218">
+                            (4912) 46-52-18
+                        </a>
+                    </div>
+                </div>
+
+                <div class="footer-contacts__phone--call">
+                    <div class="footer-contacts__phone-text--bolder">
+                        Горячая линия:
+                    </div>
+
+                    <div class="footer-contacts__phone-text--normal">
+                        <a href="tel:88003025185">
+                            8 (800) 302-51-85
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="footer-social">
+                <div class="footer-social__heading">
+                    СОЦИАЛЬНЫЕ СЕТИ
+                </div>
+
+                <div class="footer-social__items">
+                    <div class="footer-social__item">
+                        <a href="">
+                            <img src="/assets/images/footer/telegram.png" alt="telegram">
+                        </a>
+                    </div>
+                    <div class="footer-social__item">
+                        <a href="">
+                            <img src="/assets/images/footer/vk.png" alt="vk">
+                        </a>
+                    </div>
+                    <div class="footer-social__item">
+                        <a href="">
+                            <img src="/assets/images/footer/ok.png" alt="ok">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer__copyright">
+        © 2024 Фонд капитального ремонта многоквартирных домов Рязанской области
+    </div>
+</footer>
