e67733ee946733cb424e2a233740d49c971314a9
BlazEffect <kamalinigor@gmail.com>
2024-01-10 16:47:52 +0300

    Prepared pages for working with news in the admin panel

---
+++ app/Filament/Resources/NewsSectionResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\NewsSectionResource\Pages;
+use App\Filament\Resources\NewsSectionResource\RelationManagers;
+use App\Models\NewsSection;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class NewsSectionResource extends Resource
+{
+    protected static ?string $model = NewsSection::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListNewsSections::route('/'),
+            'create' => Pages\CreateNewsSection::route('/create'),
+            'edit' => Pages\EditNewsSection::route('/{record}/edit'),
+        ];
+    }
+}
