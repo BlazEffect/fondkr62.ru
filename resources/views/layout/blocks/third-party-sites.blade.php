53a2aa0be6c3e4be5fdde59147896aefc9628d36
BlazEffect <kamalinigor@gmail.com>
2024-01-11 20:28:24 +0300

    Added a block with other sites

---
+++ resources/views/layout/blocks/third-party-sites.blade.php
@@ -0,0 +1,73 @@
+<section class="third-party-sites">
+    <div class="third-party-sites__list">
+        <div class="third-party-sites__item">
+            <a href="http://ryazangov.ru/" target="_blank">
+                <img src="/assets/images/other/third-party-sites/pravitelstvo.gif"
+                     alt="Правительство Рязанской области">
+            </a>
+        </div>
+
+        <div class="third-party-sites__item">
+            <a href="http://www.mintek.ryazangov.ru/" target="_blank">
+                <img src="/assets/images/other/third-party-sites/min-tehk.gif"
+                     alt="Министерство ТЭК и ЖКХ Рязанской области">
+            </a>
+        </div>
+
+        <div class="third-party-sites__item">
+            <a href="http://gji.ryazangov.ru/" target="_blank">
+                <img src="/assets/images/other/third-party-sites/gji_sid3.gif"
+                     alt="Государственная инспекция жилищного и строительного надзора Рязанской области">
+            </a>
+        </div>
+
+        <div class="third-party-sites__item">
+            <a href="http://minstroy.ryazangov.ru/" target="_blank">
+                <img src="/assets/images/other/third-party-sites/minstroy_region.gif"
+                     alt="Минстрой Рязанской области">
+            </a>
+        </div>
+
+        <div class="third-party-sites__item">
+            <a href="http://minsoc.ryazangov.ru/" target="_blank">
+                <img src="/assets/images/other/third-party-sites/sz-logo.gif"
+                     alt="Министерство соцзащиты Рязанской области">
+            </a>
+        </div>
+
+        <div class="third-party-sites__item">
+            <a href="http://www.uprsznro.ru/" target="_blank">
+                <img src="/assets/images/other/third-party-sites/uprs.gif"
+                     alt="Управление соцзащиты Рязанской области">
+            </a>
+        </div>
+
+        <div class="third-party-sites__item">
+            <a href="http://www.fondgkh.ru/" target="_blank">
+                <img src="/assets/images/other/third-party-sites/fond-sodejstvija.gif"
+                     alt="Фонд содействия реформированию ЖКХ">
+            </a>
+        </div>
+
+        <div class="third-party-sites__item">
+            <a href="https://www.reformagkh.ru/" target="_blank">
+                <img src="/assets/images/other/third-party-sites/reforma_gkh.gif"
+                     alt="Реформа ЖКХ">
+            </a>
+        </div>
+
+        <div class="third-party-sites__item">
+            <a href="http://нашдом62.рф/ryazan/coms/add/" target="_blank">
+                <img src="/assets/images/other/third-party-sites/ndom.png"
+                     alt="НашДом">
+            </a>
+        </div>
+
+        <div class="third-party-sites__item">
+            <a href="http://www.minstroyrf.ru/" target="_blank">
+                <img src="/assets/images/other/third-party-sites/minstroy.gif"
+                     alt="Минстрой России">
+            </a>
+        </div>
+    </div>
+</section>
