f444ef0d8c9ef9f75a20415c023298156682ee20
BlazEffect <kamalinigor@gmail.com>
2024-01-12 19:14:43 +0300

    Updated links in the side menu

--- resources/views/layout/aside.blade.php
+++ resources/views/layout/aside.blade.php
@@ -2,17 +2,17 @@
     <nav class="aside-menu--first" aria-label="first-menu">
         <ul class="aside-menu--first__list">
             <li class="aside-menu--first__item">
-                <a href="" class="aside-menu--first__link">
+                <a href="/account" class="aside-menu--first__link">
                     Состояние лицевого счета
                 </a>
             </li>
             <li class="aside-menu--first__item">
-                <a href="" class="aside-menu--first__link">
+                <a href="/owners/requests" class="aside-menu--first__link">
                     Собственникам помещений
                 </a>
             </li>
             <li class="aside-menu--first__item">
-                <a href="" class="aside-menu--first__link">
+                <a href="/management/percent" class="aside-menu--first__link">
                     Органам местного самоуправления
                 </a>
             </li>
@@ -22,17 +22,17 @@
                 </a>
             </li>
             <li class="aside-menu--first__item">
-                <a href="" class="aside-menu--first__link">
+                <a href="http://rts.fondkr62.ru/" class="aside-menu--first__link">
                     Витрина закупок Фонда (РТС-Тендер)
                 </a>
             </li>
             <li class="aside-menu--first__item">
-                <a href="" class="aside-menu--first__link">
+                <a href="/contractor" class="aside-menu--first__link">
                     Подрядчикам
                 </a>
             </li>
             <li class="aside-menu--first__item">
-                <a href="" class="aside-menu--first__link">
+                <a href="https://223.rts-tender.ru/customer/lk/Deal615Registry" class="aside-menu--first__link">
                     Реестр договоров о<br>
                     проведении капремонта
                 </a>
