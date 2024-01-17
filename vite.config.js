65fd4790d4ee74593bb45efcb361de433a318691
BlazEffect <kamalinigor@gmail.com>
2024-01-17 16:22:01 +0300

    Added files to the vite assembly

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import path from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/scss/app.scss', 'resources/js/app.js'],
+            input: [
+                'resources/scss/app.scss',
+                'resources/js/app.js',
+                'resources/scss/pages/home.scss',
+                'resources/scss/pages/personal-account-email-receipts.scss',
+                'resources/scss/pages/personal-account-status.scss',
+                'resources/scss/pages/reviews.scss',
+                'resources/scss/pages/user-page.scss',
+                'resources/js/pages/find-personal-account.js',
+                'resources/js/pages/owners-premises-operator.js',
+                'resources/js/pages/personal-account-status.js',
+            ],
             refresh: true,
         }),
     ],
