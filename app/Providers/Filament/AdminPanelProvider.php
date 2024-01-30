5492fd4a721d0813fa278ec6650dc43834ec6bd4
BlazEffect <kamalinigor@gmail.com>
2024-01-30 16:49:50 +0300

    Changed the name of the admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,6 +25,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->brandName('Админ-панель')
             ->login()
             ->passwordReset()
             ->colors([
